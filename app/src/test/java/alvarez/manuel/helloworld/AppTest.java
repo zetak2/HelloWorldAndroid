7da8e22614e65ad4869448a02f681898010f4819
Jose Manuel Alvarez Martinez <jalvarez@palo-it.com>
2019-08-08 14:21:21 -0500

    Ultima version testeada correctamente

--- app/src/test/java/alvarez/manuel/helloworld/AppTest.java
+++ app/src/test/java/alvarez/manuel/helloworld/AppTest.java
@@ -1,6 +1,7 @@
 package alvarez.manuel.helloworld;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,25 +10,29 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class AppTestFail {
 
-    public static void main(String args[]) throws MalformedURLException{
-        DesiredCapabilities capabilities = new DesiredCapabilities ();
-        capabilities.setCapability("noReset","true");
-        capabilities.setCapability("deviceName","test");
-        capabilities.setCapability("platformName","android");
-        capabilities.setCapability("appPackage","alvarez.manuel.helloworld");
-        capabilities.setCapability("appActivity",".MainActivity");
+public class AppTest {
+
+    @Test
+    public void test()  throws MalformedURLException
+    {
+        System.out.println("Ejecutando clase: "+this.getClass().getSimpleName().toString());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("deviceName", "test");
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("appPackage", "alvarez.manuel.helloworld");
+        capabilities.setCapability("appActivity", ".MainActivity");
+        capabilities.setCapability("newCommandTimeout","10");
 
         AndroidDriver driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
         MobileElement el1 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/fab");
         el1.click();
-        el1.click();
         MobileElement el2 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/txtName");
-        el2.click();
-        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup");
-        el3.sendKeys("Manuel");
+        el2.setValue("Manuel");
+        driver.navigate().back();
         MobileElement el4 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/fab");
         el4.click();
         MobileElement el5 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/fab");
@@ -38,8 +43,7 @@ public class AppTestFail {
         MobileElement el7 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.TextView");
         el7.click();
 
-        Assert.assertEquals(driver.findElementById("alvarez.manuel.helloworld:id/txtName").getText(),"Fail");
-
+        Assert.assertEquals(driver.findElementById("alvarez.manuel.helloworld:id/txtName").getText(), "Manuel");
+        driver.closeApp();
     }
-
 }
--- app/src/test/java/alvarez/manuel/helloworld/AppTestFail.java
+++ app/src/test/java/alvarez/manuel/helloworld/AppTestFail.java
@@ -1,6 +1,7 @@
 package alvarez.manuel.helloworld;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,25 +10,26 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class AppTest {
+public class AppTestFail {
+    @Test
+    public void test() throws MalformedURLException{
+        System.out.println("Ejecutando clase: "+this.getClass().getSimpleName().toString());
 
-    public static void main(String args[]) throws MalformedURLException{
-        DesiredCapabilities capabilities = new DesiredCapabilities ();
-        capabilities.setCapability("noReset","true");
-        capabilities.setCapability("deviceName","test");
-        capabilities.setCapability("platformName","android");
-        capabilities.setCapability("appPackage","alvarez.manuel.helloworld");
-        capabilities.setCapability("appActivity",".MainActivity");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("deviceName", "test");
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("appPackage", "alvarez.manuel.helloworld");
+        capabilities.setCapability("appActivity", ".MainActivity");
+        capabilities.setCapability("newCommandTimeout","10");
 
         AndroidDriver driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
         MobileElement el1 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/fab");
         el1.click();
-        el1.click();
         MobileElement el2 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/txtName");
-        el2.click();
-        MobileElement el3 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup");
-        el3.sendKeys("Manuel");
+        el2.setValue("This example test must fail");
+        driver.navigate().back();
         MobileElement el4 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/fab");
         el4.click();
         MobileElement el5 = (MobileElement) driver.findElementById("alvarez.manuel.helloworld:id/fab");
@@ -38,8 +40,8 @@ public class AppTest {
         MobileElement el7 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.TextView");
         el7.click();
 
-        Assert.assertEquals(driver.findElementById("alvarez.manuel.helloworld:id/txtName").getText(),"Manuel");
-
+        Assert.assertEquals(driver.findElementById("alvarez.manuel.helloworld:id/txtName").getText(), "Manuel");
+        driver.closeApp();
     }
 
 }
